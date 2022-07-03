49e72a4e5c9ea4d4e1f51a55c268c34d6599c493
Husniddin <husniddin9944@#$>
2022-07-03 16:20:17 +0500

    project started

---
+++ main/java/com/example/test/FirsStageApplication.java
@@ -0,0 +1,13 @@
+package com.example.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsStageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirsStageApplication.class, args);
+    }
+
+}
